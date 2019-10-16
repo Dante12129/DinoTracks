54d83c5a650821339c5a4b45d31f50f178dea5c4
Dante12129 <dan9coasterdan@gmail.com>
2019-10-16 14:25:09 -0400

    Create HumanView Class

---
+++ include/views/HumanView.hpp
@@ -0,0 +1,17 @@
+//
+// Created by dan9c on 10/16/2019.
+//
+
+#ifndef DINOTRACKS_HUMANVIEW_HPP
+#define DINOTRACKS_HUMANVIEW_HPP
+
+namespace dt
+{
+    class HumanView
+    {
+
+    };
+}
+
+
+#endif //DINOTRACKS_HUMANVIEW_HPP
---
+++ src/views/HumanView.cpp
@@ -0,0 +1,10 @@
+//
+// Created by dan9c on 10/16/2019.
+//
+
+#include "views/HumanView.hpp"
+
+namespace dt
+{
+
+}
