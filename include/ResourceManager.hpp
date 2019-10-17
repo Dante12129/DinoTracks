79576852903082848bb5ac1f308e89b62e457d7b
Dante12129 <dan9coasterdan@gmail.com>
2019-10-17 17:15:03 -0400

    Create ResourceManager And Its Type Enum

---
+++ include/ResourceManager.hpp
@@ -0,0 +1,22 @@
+//
+// Created by dan9c on 10/17/2019.
+//
+
+#ifndef DINOTRACKS_RESOURCEMANAGER_HPP
+#define DINOTRACKS_RESOURCEMANAGER_HPP
+
+namespace dt
+{
+    class ResourceManager
+    {
+        // Enums
+        enum class Type {
+            Texture,
+            Font,
+            Sound
+        };
+    };
+}
+
+
+#endif //DINOTRACKS_RESOURCEMANAGER_HPP
---
+++ src/ResourceManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dan9c on 10/17/2019.
+//
+
+#include "ResourceManager.hpp"
