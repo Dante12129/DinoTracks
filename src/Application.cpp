16aeb22aba75158461ca5738b35477e739bbb20f
Dante12129 <dan9coasterdan@gmail.com>
2019-10-17 01:43:12 -0400

    Update Game Loop
    Add a variable timestep and use new HumanView methods.

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,17 +4,34 @@
 
 #include "Application.hpp"
 
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+
 namespace dt
 {
   int Application::loop()
   {
+    // To keep track of frame time
+    sf::Clock frameClock;
+    sf::Time frameDelta;
+
     while(running)
     {
-      playerView.processEvents();
+      // Get frame time and start keeping next frame time
+      frameDelta = frameClock.restart();
 
+      // Process inputs and send them to logic
+      playerView.processEvents();
+      playerView.sendCommands(gameLogic);
+
+      // Update the logical and visual state
+      gameLogic.update(frameDelta);
+      playerView.updateFrom(gameLogic);
+
+      // Actual rendering of visual state
       playerView.draw();
 
-      running = false; // TODO: implement EventManager so application can be closed.
+      running = false; // TODO: implement EventManager so application can be closed based on input.
     }
 
     return 0;
