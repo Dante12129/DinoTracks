b70c5e283a2b73d8be3fd17e5d28f950b7e5ebf8
Dante12129 <dan9coasterdan@gmail.com>
2019-11-03 23:44:01 -0500

    Create EntityBuilder

---
+++ include/EntityBuilder.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Dan on 11/3/2019.
+//
+
+#include "EntityBuilder.hpp"
+
+namespace dt
+{
+
+}
---
+++ include/EntityBuilder.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Dan on 11/3/2019.
+//
+
+#ifndef DINOTRACKS_ENTITYBUILDER_HPP
+#define DINOTRACKS_ENTITYBUILDER_HPP
+
+#include <Entity.hpp>
+
+namespace dt
+{
+    class EntityBuilder
+    {
+    public:
+
+
+    private:
+        Entity entity;
+    };
+}
+
+
+#endif //DINOTRACKS_ENTITYBUILDER_HPP
