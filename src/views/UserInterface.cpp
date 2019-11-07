55bfdeecd1a2d0fc0cfa742e9dfd42ad98074ed0
benkrup <benokrupka@gmail.com>
2019-11-07 15:08:13 -0500

    Minor formatting tweaks

--- src/views/UserInterface.cpp
+++ src/views/UserInterface.cpp
@@ -19,22 +19,22 @@ namespace dt
 		sf::Color background(160, 160, 160);
 		sf::Color border(96, 96, 96);
 		
-		uibackground.setSize(uiview.getSize());
-		uibackground.setFillColor(background);
+        uibackground.setSize(uiview.getSize());
+        uibackground.setFillColor(background);
         uibackground.setOutlineColor(border);
         uibackground.setOutlineThickness(2);
         uibackground.setPosition(0, 1);
         
         uidivider.setSize(sf::Vector2f(7, uiview.getSize().y));
-		uidivider.setFillColor(border);
+        uidivider.setFillColor(border);
         uidivider.setPosition(uiview.getSize().x/5, 0);
         
         healthbar.setSize(sf::Vector2f(uidivider.getPosition().x - 20, uiview.getSize().y/35));
-		healthbar.setFillColor(sf::Color::Red);
+        healthbar.setFillColor(sf::Color::Red);
         healthbar.setPosition(10, 5);
         
         energybar.setSize(sf::Vector2f(uidivider.getPosition().x - 20, uiview.getSize().y/35));
-		energybar.setFillColor(sf::Color::Yellow);
+        energybar.setFillColor(sf::Color::Yellow);
         energybar.setPosition(10, 15);
         
 	}
