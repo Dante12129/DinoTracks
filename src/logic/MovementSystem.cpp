fc65cc7ad054575eaadc3e8a2f344e00c80b87f5
Dante12129 <dan9coasterdan@gmail.com>
2019-11-25 13:31:02 -0500

    Basic Collision Detection

--- src/logic/MovementSystem.cpp
+++ src/logic/MovementSystem.cpp
@@ -4,6 +4,8 @@
 
 #include "MovementSystem.hpp"
 
+#include <iostream>
+
 #include <SFML/System/Vector2.hpp>
 #include "components/Component.hpp"
 #include "Entity.hpp"
@@ -21,16 +23,39 @@ namespace dt
     Component& positionComponent = entity.getComponent(POSITION);
     Component& velocityComponent = entity.getComponent(VELOCITY);
 
+    // Variables for calculating collision
+    sf::Vector2i position = positionComponent.getData().asVec2i;
+    sf::Vector2i velocity = velocityComponent.getData().asVec2i;
+    sf::Vector2i finalVelocity = velocity;
+
+    // Test collision in all four directions
+    for(int i = 0; abs(i) <= abs(velocity.x); i += velocity.x > 0 ? 1 : -1)
+    {
+      if(map.getTile(position.x + i, position.y) != 1)
+        break;
+      else
+        finalVelocity.x = i;
+    }
+    for(int i = 0; abs(i) <= abs(velocity.y); i += velocity.y > 0 ? 1 : -1)
+    {
+      if(map.getTile(position.x, position.y + i) != 1)
+        break;
+      else
+        finalVelocity.y = i;
+    }
+
     // Calculate new position based on velocity
-    sf::Vector2i newPosition = positionComponent.getData().asVec2i + velocityComponent.getData().asVec2i;
+    sf::Vector2i newPosition = position + finalVelocity;
     positionComponent.setData(ComponentData(newPosition));
 
     // Deduct energy based on terrain
     int terrain = map.getTile(newPosition.x-1, newPosition.y-1);
-    if(terrain == 0){
+    if(terrain == 0)
+    {
         enesys.adjust(entity, -20);
     }
-    else if(terrain == 1){
+    else if(terrain == 1)
+    {
         enesys.adjust(entity, -1);
     }
 
